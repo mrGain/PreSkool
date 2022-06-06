430e47a66a73de15d0a932443c2026b0ad9c6791
Rakesh Gain <rakeshgain47681@gmail.com>
2022-06-06 20:50:19 +0530

    app created

---
+++ app/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+
+class UserModel(UserAdmin):
+    list_display = ['username','user_type']
+
+admin.site.register(CustomUser,UserModel)
