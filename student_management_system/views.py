e5418ab8de8f7e323ea259bdc94392eddc6a694b
Rakesh Gain <rakeshgain47681@gmail.com>
2022-06-06 20:49:37 +0530

    initial commit

---
+++ student_management_system/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render,redirect,HttpResponse
+from django.contrib.auth import authenticate,login,logout
+
+from app.EmailBackEnd import EmailBackEnd
+
+def BASE(request):
+    return render(request,'base.html')
+
+def LOGIN(request):
+    return render(request, 'login.html')    
+
+def doLogin(request):
+    if request.method == 'POST':
+        user = EmailBackEnd.authenticate(
+            request,
+            username=request.POST.get('email'),
+            password=request.POST.get('password')
+        )
+        if user != None:
+            login(request, user)
+            user_type = user.user_type
+            
+            if user_type == '1':
+                return HttpResponse("This is HOD Panel")
+            elif user_type == '2':
+                return HttpResponse("This is Staff Panel")
+            elif user_type == '3':
+                return HttpResponse("This is Student Panel")
+            else:
+                # message
+                return redirect('login')
+
+    else:
+        return redirect('login')            
+        
